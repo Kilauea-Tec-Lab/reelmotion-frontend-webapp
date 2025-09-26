79802041acd9802885a09e32dae747a58814013d
Dekepdevelop <dekepdevelop@gmail.com>
2025-09-26 14:14:59 -0600

    Update main-layout.jsx

--- src/components/main-layout.jsx
+++ src/components/main-layout.jsx
@@ -29,7 +29,7 @@ function MainLayout() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 relative">
+    <div className="min-h-screen bg-primarioDark relative">
       {/* Frosted Glass Overlay para tutorial de Projects */}
       {showProjectsTutorial && (
         <div className="fixed inset-0 z-40 backdrop-blur-md bg-black/30">
@@ -46,7 +46,7 @@ function MainLayout() {
       />
 
       {/* Main Content Area */}
-      <main className="ml-64 pt-15 min-h-screen">
+      <main className="ml-64 pt-15 min-h-screen bg-primarioDark">
         <Outlet />
       </main>
 
