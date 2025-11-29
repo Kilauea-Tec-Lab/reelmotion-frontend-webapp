6b4fca9e3bcadea06e17637fe0668c9d6af963a3
Dekepdevelop <dekepdevelop@gmail.com>
2025-11-29 13:13:40 -0600

    Update login.jsx

--- src/auth/login.jsx
+++ src/auth/login.jsx
@@ -317,18 +317,19 @@ function Login() {
 
         {/* Center Logo & Text */}
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full max-w-4xl px-4">
-          <img
-            src="/logos/logo_reelmotion_new.png"
-            alt="Logo Reelmotion IA"
-            className="sm:w-[70%] w-[90%] mx-auto drop-shadow-2xl"
+          <div
             onClick={() => setShowAuthModal(true)}
-          />
-          <h1
-            className="text-white sm:text-3xl text-2xl montserrat-medium"
-            onClick={() => setShowAuthModal(true)}
+            className="cursor-pointer"
           >
-            Simple AI Video Creation
-          </h1>
+            <img
+              src="/logos/logo_reelmotion_new.png"
+              alt="Logo Reelmotion IA"
+              className="sm:w-[70%] w-[97%] mx-auto drop-shadow-2xl"
+            />
+            <h1 className="text-white sm:text-4xl text-2xl tracking-wider montserrat-medium">
+              Simple AI Video Creation
+            </h1>
+          </div>
           <button
             onClick={() => setShowAuthModal(true)}
             className="bg-[#DC569D] text-white text-xl px-8 py-2 mt-6 tracking-wider rounded-full font-semibold hover:bg-[#dc569ecc] hover:text-white transition-all shadow-lg hover:shadow-[#DC569D]/20 transform"
